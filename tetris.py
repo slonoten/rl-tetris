b1b02c446caeacfba554a479efa3f0dc0394d074
Max <slonoten@gmail.com>
2018-01-23 01:31:33 +0300

    Added drop, rotations and simple console UI

--- tetris.py
+++ tetris.py
@@ -2,27 +2,33 @@
 import random
 import copy
 
+
 class GameOverError(Exception):
+    "Game over exeption"
     pass
+
 
 class Tetris:
     "Class for tetris engine"
-    __figures = [[[1,1],
-                  [1,1]],
-                  [[1, 1, 0],
-                   [0, 1, 1]],
-                  [[0, 1, 1],
-                   [1, 1, 0]],
-                  [[1, 1, 1],
-                   [1, 0, 0]],
-                  [[1, 1, 1],
-                   [0, 0, 1]],
-                  [[1, 1, 1, 1]]]
+    __figure_patterns = [[[1, 1],
+                  [1, 1]],
+                 [[1, 1, 0],
+                  [0, 1, 1]],
+                 [[0, 1, 1],
+                  [1, 1, 0]],
+                 [[1, 1, 1],
+                  [1, 0, 0]],
+                 [[1, 1, 1],
+                  [0, 0, 1]],
+                 [[1, 1, 1, 1]]]
 
-    def __init__(self, columns_num = 10, rows_num = 20):
+    def __init__(self, columns_num=10, rows_num=20):
+        self.__build_figures()
+        self.__figure = None
         self.__columns_num = columns_num
         self.__rows_num = rows_num
-        self.__well = [[0] * self.__columns_num for i in range(self.__rows_num)]
+        self.__well = [
+            [0] * self.__columns_num for i in range(self.__rows_num)]
         self.__figure_row = -1
         self.__figure_column = 0
         self.__place_new_figure()
@@ -31,7 +37,7 @@ class Tetris:
 
     def step(self, command):
         """ Executes command.
-        
+
         Args:
             command: integer representig a command
                 0 - no command
@@ -45,7 +51,9 @@ class Tetris:
         if self.__is_game_over:
             raise GameOverError()
 
-        if self.__check_is_intersection(self.__figure_row + 1, self.__figure_column):
+        self.__apply_command(command)
+
+        if self.__check_is_intersection(self.__figure, self.__figure_row + 1, self.__figure_column):
             if self.__figure_row < 0:
                 # Game over
                 self.__is_game_over = True
@@ -54,66 +62,91 @@ class Tetris:
                 self.__remove_full_rows()
                 self.__place_new_figure()
             return copy.deepcopy(self.__well), self.__score, self.__is_game_over
-        else:   
-            self.__apply_command(command)
+        else:
             self.__figure_row = self.__figure_row + 1
-        
-        return self._compose_well(), self.__score, False 
+
+        return self._compose_well(), self.__score, False
+
+    @staticmethod
+    def rotate(figure):
+        new_figure = [[0] * len(figure) for i in range(len(figure[0]))]
+        width = len(figure[0])
+        for i in range(len(figure)):
+            for j in range(width):
+                new_figure[j][i] = figure[i][width - j - 1]
+        return new_figure
+
+    def __build_figures(self):
+        self.__figures = []
+        for figure in self.__figure_patterns:
+            figures = [figure]
+            for i in range(3):
+                figures.append(Tetris.rotate(figures[i]))
+            self.__figures.append(figures)
 
     def __place_new_figure(self):
         "Set position for new figure"
-        self.__figure = copy.deepcopy(self.__figures[random.randrange(len(self.__figures))])
+        self.__figure_idx = random.randrange(len(self.__figures))
+        self.__figure_angle = 0
+        self.__figure = self.__figures[self.__figure_idx][self.__figure_angle]
         self.__figure_row = -1
-        self.__figure_column = (self.__columns_num - len(self.__figures[0])) // 2
+        self.__figure_column = (self.__columns_num -
+                                len(self.__figures[0])) // 2
 
-    def __check_is_intersection(self, row_num, column_num):
-        # Check bottom 
-        if row_num + len(self.__figure) > self.__rows_num:
+    def __check_is_intersection(self, figure, row_num, column_num):
+        # Check bottom
+        if row_num + len(figure) > self.__rows_num:
             return True
         # Check sides
-        if column_num < 0 or column_num + len(self.__figure[0]) > self.__columns_num:
+        if column_num < 0 or column_num + len(figure[0]) > self.__columns_num:
             return True
         # Check intersetion with well
-        for i in range(len(self.__figure)):
-            for j in range(len(self.__figure[i])):
-                if self.__well[row_num + i][column_num + j] > 0 and self.__figure[i][j] > 0:
+        for i in range(len(figure)):
+            for j in range(len(figure[i])):
+                if self.__well[row_num + i][column_num + j] > 0 and figure[i][j] > 0:
                     return True
         return False
 
-    def __freeze_figure(self, well, coef = 1):
+    def __freeze_figure(self, well, fill_with=1):
         for i in range(len(self.__figure)):
             for j in range(len(self.__figure[i])):
-                well[self.__figure_row + i][self.__figure_column + j] = self.__figure[i][j] * coef
+                if self.__figure[i][j] > 0:
+                    well[self.__figure_row + i][self.__figure_column + j] = fill_with
 
     def __apply_command(self, command):
-        if command == 0:
-            return
-        if command in [1,2]:
+        if command in [1, 2]:
+            # move
             shift = -1 if command == 1 else 1
-            if not self.__check_is_intersection(self.__figure_row, self.__figure_column + shift):
+            if not self.__check_is_intersection(self.__figure, self.__figure_row, self.__figure_column + shift):
                 self.__figure_column = self.__figure_column + shift
+        elif command == 3:
+            # rotate
+            new_angle = (self.__figure_angle + 1) & 3
+            new_figure = self.__figures[self.__figure_idx][new_angle]
+            if not self.__check_is_intersection(new_figure, self.__figure_row, self.__figure_column):
+                self.__figure = new_figure
+                self.__figure_angle = new_angle
+        elif command == 4:
+            while not self.__check_is_intersection(self.__figure, self.__figure_row + 1, self.__figure_column):
+                self.__figure_row = self.__figure_row + 1
+
 
     def __remove_full_rows(self):
-        pass
+        rows_removed = 0
+        figure_height = len(self.__figure)
+        for i in range(figure_height):
+            row_to_check = self.__figure_row + figure_height - i - 1
+            if sum(self.__well[row_to_check]) == self.__columns_num:
+                self.__well.pop(row_to_check)
+                rows_removed = rows_removed + 1
+        self.__score = self.__score + rows_removed * 100
+        for i in range(rows_removed):
+            self.__well.insert(0, [0] * self.__columns_num)
 
     def _compose_well(self):
         well = copy.deepcopy(self.__well)
         self.__freeze_figure(well, 2)
         return well
 
-def print_well(well):
-    for i in range(len(well)):
-        row = '|' + ''.join([(' ' if x == 0 else 'X') for x in well[i]]) + '|'
-        print(row)
-    print('+' + '-' * len(well[0]) + '+')
 
 t = Tetris()
-while True:
-    print('Command (0-3)?')
-    r = t.step(int(input()))
-    if r[2]:
-        print('GAME OVER')
-        break
-    print_well(r[0])
-    print('Score: ', r[1])
-
---
+++ tetris_ui.py
@@ -0,0 +1,51 @@
+"Simple UI to test tetris"
+
+import curses
+
+from time import sleep
+from tetris import Tetris
+
+
+def print_well(stdscr, well):
+    well_rows_n = len(well)
+    for i in range(well_rows_n):
+        row = '|' + ''.join([(' ' if x == 0 else 'X') for x in well[i]]) + '|'
+        stdscr.addstr(i, 0, row)
+    stdscr.addstr(well_rows_n, 0, '+' + '-' * len(well[0]) + '+')
+
+
+def convert_to_command(ch):
+    if ch < 0:
+        return 0
+    if ch == curses.KEY_LEFT:
+        return 1
+    if ch == curses.KEY_RIGHT:
+        return 2
+    if ch == curses.KEY_UP:
+        return 3
+    if ch == curses.KEY_DOWN:
+        return 4
+    if ch == 27:
+        return -1
+    return 0
+
+def game(stdscr):
+    stdscr.clear()
+    stdscr.nodelay(True)
+    tetris = Tetris()
+    is_game_over = False
+    while not is_game_over:
+        command = convert_to_command(stdscr.getch())
+        if command < 0:
+            break
+        well, score, is_game_over = tetris.step(command)
+        print_well(stdscr, well)
+        well_columns = len(well[0])
+        stdscr.addstr(0, well_columns + 4, 'SCORE:')
+        stdscr.addstr(1, well_columns + 4, str(score))
+        sleep(0.5)
+        stdscr.refresh()
+    
+curses.wrapper(game)
+    
+#stdscr.getkey()
